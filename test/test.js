d420d7392c429ec8c7991cce118b19e0e0e936b9
charleywong <charleywong@users.noreply.github.com>
2017-03-29 18:30:29 +1100

    Add files via upload
    Tests writtten for step1.js

--- test/test.js
+++ test/test.js
@@ -1,10 +1,54 @@
-var assert = require('assert');
 
-// Sample test code
-describe('Array', function () {
-	describe('#indexOf()', function () {
-		it('should return -1 when the value is not present', function () {
-			assert.equal(-1, [1, 2, 3].indexOf(4));
-		});
+const _ = require('lodash');
+const assert = require('assert');
+const {parseInput} = require('../src/step1.js');
+var {fetchData} = require('../src/step2.js');
+var buildTable = require('../src/step3.js');
+var {
+	calculate,
+	return_number,
+	return_percentage,
+	avg_return,
+	cumulative_return
+} = require('../src/step4.js');
+
+//test for input parsing
+describe('parseInt', function() {
+	let parameters = {
+			"InstrumentID": "ABP.AX",
+			"ListOfVar": ["CM_Return", "AV_Return"],
+			"UpperWindow": 5,
+			"LowerWindow": 3,
+			"DateOfInterest": "10/12/2012"
+		}
+	let {InstrumentID, ListOfVar, UpperWindow, LowerWindow, DateOfInterest} = parseInput(parameters);
+
+	it('InstrumentID is a string', function() {
+		assert.equal(_.isString(InstrumentID), true);
 	});
+	it('ListOfVar is an array', function() {
+		assert.equal(_.isArray(ListOfVar), true);
+	});
+	//check type of each var in json object is correct
+	it('UpperWindow is a number', function() {
+		assert.equal(_.isInteger(UpperWindow), true);
+	});
+	it('LowerWindow is a number', function() {
+		assert.equal(_.isInteger(LowerWindow), true);
+	});
+	it('DateOfInterest is a date', function() {
+		assert.equal(_.isDate(DateOfInterest), true);
+	});
+
 });
+
+//test for fetchData
+//check that 400 error or 404 error is thrown if insufficient data
+// describe('fetchData', function() {
+// 	let {
+// 		InstrumentID,
+// 		UpperWindow,
+// 		LowerWindow,
+// 		DateOfInterest
+// 	} = parameters;
+// });
