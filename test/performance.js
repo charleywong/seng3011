478919ebd17b04cc4768d6bdccd11d81158b9512
Minh Cung <z3493000@student.unsw.edu.au>
2017-04-26 13:29:30 +1000

    Fixed performance Test

--- test/performance.js
+++ test/performance.js
@@ -5,38 +5,55 @@ const {
 } = require('chai');
 require('isomorphic-fetch');
 
-const MULTI_IDS_TEST_URLS = [
-	// Correct parameters with 1 company ABP.AX
-	'http://ec2-54-160-211-66.compute-1.amazonaws.com:3000/api/company_returns?InstrumentID=ABP.AX&ListOfVar=CM_Return&ListOfVar=AV_Return&UpperWindow=5&LowerWindow=3&DateOfInterest=10%2F12%2F2012',
-	// Correct parameters with 3 companies AAPL, MSFT, FB
-	'http://ec2-54-160-211-66.compute-1.amazonaws.com:3000/api/company_returns?InstrumentID=AAPL,MSFT,FB&ListOfVar=CM_Return&ListOfVar=AV_Return&UpperWindow=5&LowerWindow=3&DateOfInterest=10%2F12%2F2012',
-	// Correct parameters with 6 companies AAPL,MSFT,AMZN,GOOGL,FB,BABA
-	'http://ec2-54-160-211-66.compute-1.amazonaws.com:3000/api/company_returns?InstrumentID=AAPL,MSFT,AMZN,GOOGL,FB,BABA&ListOfVar=CM_Return&ListOfVar=AV_Return&UpperWindow=5&LowerWindow=3&DateOfInterest=10%2F12%2F2012'
-];
+const API_URL = 'http://ec2-54-160-211-66.compute-1.amazonaws.com:3000/api/company_returns?';
  
 
 describe('Multiple IDs performance testing with one request',  ()=> {
 	describe('# Request returns for one company',  () => {	
 		it('should get response within 5000ms', function(done) {
 			this.timeout(5000);
-			fetch(MULTI_IDS_TEST_URLS[0])
+			let parameters = {
+				InstrumentID: 'ABP.AX',
+				ListOfVar: ['CM_Return', 'AV_Return'],
+				UpperWindow: 5,
+				LowerWindow: 3,
+				DateOfInterest: '10/12/2012'
+			}
+			fetch(API_URL + encodeURIComponent(parameters))
 				.then(response => response.json())
+				.then(json => done())
 				.catch(err => done(err))
 		});
 	})
 	describe('# Request returns for 3 companies',  () => {	
-		it('should get response within 5000ms', function(done) {
+		it('should get response within 5000ms', function (done) {
+			let parameters = {
+				InstrumentID: 'AAPL,MSFT,FB',
+				ListOfVar: ['CM_Return', 'AV_Return'],
+				UpperWindow: 5,
+				LowerWindow: 3,
+				DateOfInterest: '10/12/2012'
+			}
 			this.timeout(5000);
-			fetch(MULTI_IDS_TEST_URLS[2])
+			fetch(API_URL + encodeURIComponent(parameters))
 				.then(response => response.json())
+				.then(json => done())
 				.catch(err => done(err))
 		});
 	})
 	describe('# Request returns for 6 companies',  () => {	
-		it('should get response within 5000ms', function(done) {
+		it('should get response within 5000ms', function (done) {
+			let parameters = {
+				InstrumentID: 'AAPL,MSFT,AMZN,GOOGL,FB,BABA',
+				ListOfVar: ['CM_Return', 'AV_Return'],
+				UpperWindow: 5,
+				LowerWindow: 3,
+				DateOfInterest: '10/12/2012'
+			}
 			this.timeout(5000);
-			fetch(MULTI_IDS_TEST_URLS[1])
+			fetch(API_URL + encodeURIComponent(parameters))
 				.then(response => response.json())
+				.then(json => done())
 				.catch(err => done(err))
 		});
 	})
