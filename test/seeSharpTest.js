22dce3222db909f42de3cea3103111184f23e729
charleywong <charleywong@users.noreply.github.com>
2017-04-27 14:45:18 +1000

    Fixed up format

--- test/seeSharpTest.js
+++ test/seeSharpTest.js
@@ -33,7 +33,6 @@ describe('Testing that incorrect input leads to incorrect output',  ()=> {
 	describe('# Invalid InstrumentID',  () => {	
 		it('should display an error message saying InstrumentID is missing or URL is not in valid format', function(done) {
 			let parameters = {
-				InstrumentID: 'ABP-AX',
 				DateOfInterest: '2012-12-10',
 				List_of_Var: ['CM_Return'],
 				Upper_window: 5,
@@ -93,7 +92,7 @@ describe('Testing that incorrect input leads to incorrect output',  ()=> {
 	describe('# Invalid List_of_Var',  () => {
 		//this doesn't pass because they made it so that if you only specify one return and misspell it
 		//it interprets  it as no specified input for List_of_Var
-		it('should display an error message saying DateOfInterest is invalid', function(done) {
+		it('should display an error message saying List_of_Var is invalid', function(done) {
 			let parameters = {
 				InstrumentID: 'ABP.AX',
 				DateOfInterest: '2012-12-10',
@@ -111,8 +110,8 @@ describe('Testing that incorrect input leads to incorrect output',  ()=> {
 				.catch(err => done(err))
 		});
 	})
-	describe('# Invalid List_of_Var',  () => {
-		it('should display an error message saying DateOfInterest is missing', function(done) {
+	describe('# Missing List_of_Var',  () => {
+		it('should display an error message saying List_of_Var is missing', function(done) {
 			let parameters = {
 				InstrumentID: 'ABP.AX',
 				DateOfInterest: '2012-12-10',
@@ -129,13 +128,49 @@ describe('Testing that incorrect input leads to incorrect output',  ()=> {
 				.catch(err => done(err))
 		});
 	})
-	describe('# Missing Lower_window',  () => {	
+	describe('# Invalid Upper_window',  () => {	
+		it('should display an error message Upper_window is invalid', function(done) {
+			let parameters = {
+				InstrumentID: 'ABP.AX',
+				DateOfInterest: '2012-12-10',
+				List_of_Var: ['CM_Return'],
+				Upper_window: "five",
+				Lower_window: 3			
+			}
+			fetch(API_URL + querystring.stringify(parameters, '/', '/'))
+				.then(response => response.json())
+				.then(function(json) {
+					if (json.Errors == "Upper Window Value is Invalid") {
+						done();
+					}
+				})
+				.catch(err => done(err))
+		});
+		it('should display an error message Upper_window is invalid', function(done) {
+			let parameters = {
+				InstrumentID: 'ABP.AX',
+				DateOfInterest: '2012-12-10',
+				List_of_Var: ['CM_Return'],
+				Upper_window: -5,
+				Lower_window: 3			
+			}
+			fetch(API_URL + querystring.stringify(parameters, '/', '/'))
+				.then(response => response.json())
+				.then(function(json) {
+					if (json.Errors == "Upper Window Value is Invalid") {
+						done();
+					}
+				})
+				.catch(err => done(err))
+		})
+	})	
+	describe('# Missing Upper_window',  () => {	
 		it('should display an error message saying \'invalid\' format', function(done) {
 			let parameters = {
 				InstrumentID: 'ABP.AX',
 				DateOfInterest: '2012-12-10',
 				List_of_Var: ['CM_Return'],
-				Upper_window: 5				
+				Lower_window: 5				
 			}
 			fetch(API_URL + querystring.stringify(parameters, '/', '/'))
 				.then(response => response.json())
@@ -165,38 +200,36 @@ describe('Testing that incorrect input leads to incorrect output',  ()=> {
 				})
 				.catch(err => done(err))
 		});
-	})
-	describe('# Missing Upper_window',  () => {	
-		it('should display an error message saying \'invalid\' input', function(done) {
+		it('should display an error message Lower_window is invalid', function(done) {
 			let parameters = {
 				InstrumentID: 'ABP.AX',
 				DateOfInterest: '2012-12-10',
 				List_of_Var: ['CM_Return'],
-				Lower_window: 5				
+				Upper_window: 5,
+				Lower_window: -3				
 			}
 			fetch(API_URL + querystring.stringify(parameters, '/', '/'))
 				.then(response => response.json())
 				.then(function(json) {
-					if (json.Errors == "Request URL is Not in a Valid Format") {
+					if (json.Errors == "Lower Window Value is Invalid") {
 						done();
 					}
 				})
 				.catch(err => done(err))
 		});
 	})
-	describe('# Invalid Upper_window',  () => {	
-		it('should display an error message Lower_window is invalid', function(done) {
+	describe('# Missing Lower_window',  () => {	
+		it('should display an error message saying \'invalid\' format', function(done) {
 			let parameters = {
 				InstrumentID: 'ABP.AX',
 				DateOfInterest: '2012-12-10',
 				List_of_Var: ['CM_Return'],
-				Upper_window: "five",
-				Lower_window: 3			
+				Upper_window: 5				
 			}
 			fetch(API_URL + querystring.stringify(parameters, '/', '/'))
 				.then(response => response.json())
 				.then(function(json) {
-					if (json.Errors == "Upper Window Value is Invalid") {
+					if (json.Errors == "Request URL is Not in a Valid Format") {
 						done();
 					}
 				})
