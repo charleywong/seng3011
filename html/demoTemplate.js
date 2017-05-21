8a8b1272b8f0be2da7b387fc757ccb001a74813b
Andrew <matvign4182@outlook.com>
2017-05-21 22:10:13 +1000

    updated demo template

---
+++ html/demoTemplate.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+    $("#demoContent").hide();
+    
+    $("#startDemo").click(function () {
+        $("#demoContent").fadeIn();
+        $("#startContent").hide();
+    });
+    
+    $("#searchA").keyup(function () {
+        var filter = $(this).val();
+        $('a', 'div#myListA').each(function () {
+            if ($(this).text().search(new RegExp(filter, "i")) < 0) {
+                $(this).hide();
+            } else {
+                $(this).show()
+            }
+        });
+    });
+});
