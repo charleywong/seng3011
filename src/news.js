bf53dccb2c52ac9f93a6276fab8c87886df00825
Minh Cung <z3493000@student.unsw.edu.au>
2017-05-22 14:37:45 +1000

    News + fixed company returns

--- src/news.js
+++ src/news.js
@@ -8,11 +8,12 @@ const myCache = new NodeCache();
 const COOL_BANANA_ENDPOINT = "https://nickr.xyz/coolbananas/api";
 const TOPICS_LIST_ENDPOINT = COOL_BANANA_ENDPOINT + "/topics";
 const NOTFOUND_API_ENDPOINT = "http://159.203.160.38:8080/Seng3011/apiv2/";
+const GOOGLE_NEWSRSS_ENDPOINT =
+    "https://www.google.com/finance/company_news?output=rss&q=";
+const parser = require("rss-parser");
 
 let responseHandler = response =>
-    response.status >= 400
-        ? Promise.resolve({ newsDataSet: [] })
-        : response.json();
+    response.status >= 400 ? Promise.resolve({}) : response.text();
 
 /**
  * Get News topics code from COOL_BANANA_ENDPOINT
@@ -29,39 +30,65 @@ let getTopicCodes = async () =>
  */
 let getNews = async parameters => {
     // Get Topics Code from cool bananas
-    let topicCodes = myCache.get("topic");
-    if (!topicCodes) {
-        topicCodes = await getTopicCodes();
-        myCache.set("topic", topicCodes);
-    }
+    // let topicCodes = myCache.get("topic");
+    // if (!topicCodes) {
+    //     topicCodes = await getTopicCodes();
+    //     myCache.set("topic", topicCodes);
+    // }
     // Get news from 404 Not Found API
-    let response = myCache.get("news" + JSON.stringify(parameters));
+    let symbols = _.map(
+        parameters.InstrumentID,
+        v => "ASX:" + v.replace(".AX", "")
+    );
+    let response = myCache.get("news" + JSON.stringify(symbols));
     if (response) return Promise.resolve(response);
 
-    let data = {
-        "start-date": moment(parameters.DateOfInterest)
-            .subtract(parameters.LowerWindow, "days")
-            .toISOString(),
-        "end-date": moment(parameters.DateOfInterest)
-            .add(parameters.UpperWindow, "days")
-            .toISOString(),
-        instrumentId: parameters.InstrumentID.map(v => "RIC_" + v).join(","),
-        "topic-codes": topicCodes.join(",")
-    };
+    let promises = [];
+    for (let symbol of symbols) {
+        let url = GOOGLE_NEWSRSS_ENDPOINT + symbol;
+        promises.push(
+            fetch(url).then(responseHandler).then(
+                rss =>
+                    new Promise((resolve, reject) => {
+                        parser.parseString(rss, (err, parsed) => {
+                            if (err) reject(err);
+                            else resolve(_.extend({ id: symbol }, parsed));
+                        });
+                    })
+            )
+        );
+    }
+    let json = await Promise.all(promises);
+    try {
+        myCache.set("news" + JSON.stringify(symbols), json);
+        return json;
+    } catch (err) {
+        return [];
+    }
+    // let data = {
+    //     "start-date": moment(parameters.DateOfInterest)
+    //         .subtract(parameters.LowerWindow, "days")
+    //         .toISOString(),
+    //     "end-date": moment(parameters.DateOfInterest)
+    //         .add(parameters.UpperWindow, "days")
+    //         .toISOString(),
+    //     instrumentId: parameters.InstrumentID.map(v => "RIC_" + v).join(","),
+    //     "topic-codes": topicCodes.join(",")
+    // };
 
-    return fetch(NOTFOUND_API_ENDPOINT + qs.stringify(data, "/", "/"))
-        .then(responseHandler)
-        .then(json => {
-            try {
-                myCache.set(
-                    "news" + JSON.stringify(parameters),
-                    json.newsDataSet
-                );
-                return json.newsDataSet;
-            } catch (err) {
-                return [];
-            }
-        });
+    // return fetch(NOTFOUND_API_ENDPOINT + qs.stringify(data, "/", "/"))
+    //     .then(responseHandler)
+    //     .then(json => {
+    //         try {
+    //             myCache.set(
+    //                 "news" + JSON.stringify(parameters),
+    //                 json.newsDataSet
+    //             );
+    //             return json.newsDataSet;
+    //         } catch (err) {
+    //             return [];
+    //         }
+    //     });
 };
 
 module.exports = {
